129738ae7333d2bdeb0b26812bc7a9af21995493
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2026-09-03 11:26:16 +0300

    Products Api Completed

---
+++ example6backend/app/Http/Resources/ProductResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'slug' => $this->slug,
+            'name' => $this->name,
+            'category_id ' => $this->category_id,
+            'price' => $this->price,
+            'quantity' => $this->quantity,
+            'description' => $this->description,
+            'thumbnail' => $this->thumbnail,
+            'colors' => $this->colors,
+            'sizes' => $this->sizes
+        ];
+    }
+}
